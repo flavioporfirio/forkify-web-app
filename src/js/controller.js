435dd5c3111e0f7314a2baea2aec4a101278842d
flavioporfirio <flavioporfirio86@gmail.com>
2023-10-12 17:27:40 -0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -132,6 +132,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -140,8 +144,7 @@ const init = function () {
   searchView.addHandlerSeach(controlSeachResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-
-  console.log('Welcome');
+  newFeature();
 };
 
 init();

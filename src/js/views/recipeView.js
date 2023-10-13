c30f192dc42f195451226486e58b5128be696963
flavioporfirio <flavioporfirio86@gmail.com>
2023-10-13 17:07:06 -0300

    Remove fraction lib

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import View from './View.js';
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
 
 import icons from 'url:../../img/icons.svg';
 
@@ -132,9 +132,7 @@ class RecipeView extends View {
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
         </svg>
-        <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity) : ''
-        }</div>
+        <div class="recipe__quantity">${ing.quantity ? ing.quantity : ''}</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
           ${ing.description}
